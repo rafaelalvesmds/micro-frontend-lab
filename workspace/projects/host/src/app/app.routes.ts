3d90142197197122d9c226f1ace14fad63138f08
rafaelalvesmds <rafaelalvesmds.dev@gmail.com>
2025-03-14 11:22:34 -0300

    configure routes

--- workspace/projects/host/src/app/app.routes.ts
+++ workspace/projects/host/src/app/app.routes.ts
@@ -1,3 +1,29 @@
+import { loadRemoteModule } from '@angular-architects/native-federation';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'ecobit', loadComponent: () => {
+            return loadRemoteModule('ecobit', './Component')
+                .then(m => m.AppComponent);
+        }
+    },
+    {
+        path: 'nexum', loadComponent: () => {
+            return loadRemoteModule('nexum', './Component')
+                .then(m => m.AppComponent);
+        }
+    },
+    {
+        path: 'redflux', loadComponent: () => {
+            return loadRemoteModule('redflux', './Component')
+                .then(m => m.AppComponent);
+        }
+    },
+    {
+        path: 'stratos', loadComponent: () => {
+            return loadRemoteModule('stratos', './Component')
+                .then(m => m.AppComponent);
+        }
+    }
+];
